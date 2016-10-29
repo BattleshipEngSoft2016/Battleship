a3b7d38838a6497a390f2bdc1138690d0840d0b4
Julio Andrade <julio_10_andrade@hotmail.com>
2016-10-29 13:29:12 -0200

    Ajustes de Layout inicio das models de banco

---
+++ Jogo/Jogo/Controllers/NivelController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Jogo.Models;
+
+namespace Jogo.Controllers
+{
+    public class NivelController : Controller
+    {
+        //
+        // GET: /NIvel/
+
+        public ActionResult Index()
+        {
+
+            ViewBag.Niveis = new NiveisContext().Niveis;
+
+            return View();
+        }
+
+
+        public JsonResult Cadastrar()
+        {
+            try
+            {
+                using (var ctx = new NiveisContext())
+                {
+                    Nivel stud = new Nivel() { };
+                    ctx.Niveis.Add(stud);
+                    ctx.SaveChanges();
+                }
+
+                return Json(new { Sucesso = true });
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Sucesso = false });
+            }
+
+        }
+
+    }
+}
---
+++ Jogo/Jogo/Controllers/SkinController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Jogo.Models;
+
+namespace Jogo.Controllers
+{
+    public class SkinController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            ViewBag.Skins = null;
+
+            var julio = new UsersContext();
+
+            var xxx = julio.UserProfiles;
+
+
+            return View();
+        }
+
+        public JsonResult Cadastrar() {
+
+
+
+            return null;
+        
+        }
+
+    }
+}
