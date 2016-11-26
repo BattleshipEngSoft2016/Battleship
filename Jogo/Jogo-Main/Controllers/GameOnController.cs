2beee189fcb15dbffb1fb9e2649e6a30039eefc9
Julio Andrade <julio_10_andrade@hotmail.com>
2016-11-26 17:14:50 -0200

    Controllers Novas sem html

---
+++ Jogo/Jogo-Main/Controllers/GameOnController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jogo_Main.Controllers
+{
+    public class GameOnController : Controller
+    {
+        
+        public ActionResult Index(int nivelId)
+        {
+
+            if(nivelId == 0)
+            {
+
+
+            }
+
+
+            return View();
+        }
+
+    }
+}
