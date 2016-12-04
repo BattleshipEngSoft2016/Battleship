2a92e41d5b0406ba894da6458b14521d8e65344c
meiryasuda <meiry.yasuda@gmail.com>
2016-12-04 11:59:23 -0200

    terminado loja e ajustes gerais

--- app/javascripts/home.js
+++ app/javascripts/home.js
@@ -13,3 +13,35 @@ $('#home').find('.js-btn-shop').on('click', function() {
   levels.addClass('active');
   back.addClass('active');
 });
+
+$('#home').find('.btn-close').on('click', function() {
+  var divContent = $(this).closest('.sh-div-default');
+  var back = $('#home').find('.lyt-main');
+  divContent.removeClass('active');
+  divContent.addClass('not-active');
+  if($(this).closest('div').attr('class').indexOf('modal-loja') == -1) back.removeClass('active');
+});
+
+$('#home').find('.pirata').on('click', function() {
+  var modal = $('#home').find('.modal-pirates');
+  modal.removeClass('not-active');
+  modal.addClass('active');
+});
+
+$('#home').find('.espacial').on('click', function() {
+  var modal = $('#home').find('.modal-space');
+  modal.removeClass('not-active');
+  modal.addClass('active');
+});
+
+$('#home').find('.coca').on('click', function() {
+  var modal = $('#home').find('.modal-coca');
+  modal.removeClass('not-active');
+  modal.addClass('active');
+});
+
+$('#home').find('.sushi').on('click', function() {
+  var modal = $('#home').find('.modal-sushi');
+  modal.removeClass('not-active');
+  modal.addClass('active');
+});
